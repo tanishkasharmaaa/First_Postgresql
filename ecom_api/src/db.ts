4edf723a502dcbc2e96349f036873e677043abc6
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2026-02-22 15:46:38 +0530

    updated

--- ecom_api/src/db.ts
+++ ecom_api/src/db.ts
@@ -2,9 +2,13 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+// const isProduction = process.env.NODE_ENV === "production";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.query("SELECT current_database()")
